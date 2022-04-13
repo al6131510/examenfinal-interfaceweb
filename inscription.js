c05f4f98675f984fb9b5913ebfc079255c8d0f68
al6131510 <93780290+al6131510@users.noreply.github.com>
2022-04-13 10:18:42 -0400

    Update inscription.js

--- inscription.js
+++ inscription.js
@@ -4,7 +4,20 @@
  */
 function afficherCours(listeCours) {
     listeCours.forEach(cours => {
-        $('#cours').append(`<option>${cours.code} ${cours.nom}</option>`)
+        $('#cours').append(`<option value="${cours.code}">${cours.code} ${cours.nom}</option>`)
+        $('#CoursOfferts').append(`
+            <li class="card col-4 me-2">
+                <div class="card-body">
+                    <!-- Code et titre du cours -->
+                    <h3 class="card-title" id="Title">${cours.code} ${cours.nom}</h3>
+                    <!-- Description du cours -->
+                    <p class="card-text">${cours.description}</p>
+                        <!-- Liste des étudiants format : nom (code) -->
+                        <h3 class="card-title">Étudiant.e.s inscrit.e.s</h3>
+                        <ul class="list-group list-group-flush" id="${cours.code}listeEtudiant">
+                        </ul>
+                </div>
+            </li>`);
     });
 }
 
@@ -19,11 +32,8 @@ afficherCours(listeCoursJSON)
 //constructeur de l'objet Etudiant
 function Etudiant(etudiantCode = '00000', etudiantNom = 'Programme') {
     this.etudiantCode = etudiantCode,
-        this.nom = etudiantNom
+        this.etudiantNom = etudiantNom
 };
-
-//liste d'étudiants
-let listeEtudiant = [];
 
 /**
  * lorsqu'on appuit sur soumettre
@@ -31,27 +41,11 @@ let listeEtudiant = [];
  * et crée une carte avec les données de l'étudiant et du cours
  */
 $('form').submit(function (event) {
-
-
     let $nomEtudiant = $('#nom').val();
     let $codeEtudiant = $('#code').val();
+    let $codeCours = $('#cours').val();
     event.preventDefault(event);
     const etudiant = new Etudiant($codeEtudiant, $nomEtudiant)
-    listeEtudiant.push(etudiant);
-    listeEtudiant.forEach((etudiant) => {
-        $('#CoursOfferts').append(`${cours.nom}
-    <li class="card col-4 me-2">
-        <div class="card-body">
-            <!-- Code et titre du cours -->
-            <h3 class="card-title">${cours.code} ${cours.nom}</h3>
-            <!-- Description du cours -->
-            <p class="card-text">${cours.description}</p>
-                <!-- Liste des étudiants format : nom (code) -->
-                <h3 class="card-title">Étudiant.e.s inscrit.e.s</h3>
-                <ul class="list-group list-group-flush" id="listeEtudiant">
-                    <li class="list-group-item">${etudiant.nomEtudiant} ${etudiant.codeEtudiant}</li>
-                </ul>
-        </div>
-    </li>`)
-    })
+    $(`#${$codeCours}listeEtudiant`).append(`
+        <li class="list-group-item">${etudiant.etudiantNom} (${etudiant.etudiantCode})</li>`)
 });
